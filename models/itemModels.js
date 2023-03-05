0c160ace592f3f2a4e2a907b904623653cb1de34
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-03-05 21:09:01 +0100

    items available object creation

---
+++ models/itemModels.js
@@ -0,0 +1,67 @@
+const items = [
+  {
+    id: 1,
+    item: 'iPhone 15 pro max',
+    description: 'Apple company latest flagish phone',
+    amount: 1500,
+  },
+  {
+    id: 2,
+    item: 'Microsoft surface pro 7',
+    description: 'Microsoft latest flagish PC for developers',
+    amount: 2100,
+  },
+  {
+    id: 3,
+    item: 'Bible (king James Version)',
+    description: 'The Holy Bible of GOD in king James version',
+    amount: 40,
+  },
+  {
+    id: 4,
+    item: 'Macbook pro 2023',
+    description:
+      'Apple company latest flagish PC for high end design and web development',
+    amount: 2800,
+  },
+  {
+    id: 5,
+    item: 'Airpods pro 2',
+    description: 'Apple company latest flagish quality airpods',
+    amount: 250,
+  },
+  {
+    id: 6,
+    item: 'Mercedez Benz G-wagon',
+    description: 'The new s-class model from Benz',
+    amount: 88000,
+  },
+  {
+    id: 7,
+    item: 'Ring Light (RGB)',
+    description: 'Multi-colored ring light.',
+    amount: 80,
+  },
+  {
+    id: 8,
+    item: 'Xiaomi Redmi Note 12 pro',
+    description: 'Xiaomi alternate flagish phone',
+    amount: 1000,
+  },
+  {
+    id: 9,
+    item: 'Starlink (wifi-router)',
+    description: 'New wifi-router from space X',
+    amount: 3500,
+  },
+  {
+    id: 10,
+    item: 'Tesla model X (S-plaid)',
+    description: 'Latest tesla model electric car',
+    amount: 100000,
+  },
+];
+
+// console.log(Object.keys(data)[0]);
+
+module.exports = items;
