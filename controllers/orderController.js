2fe79a87ac41a3bd5eb14c48a842eec6111e424b
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-03-07 15:10:08 +0100

    implement placeOrder middleware func

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -48,3 +48,38 @@ exports.getInfoAboutChatbot = async (req, res, next) => {
     next(new Error(`Try accessing this page at a later time ${error}`));
   }
 };
+
+exports.placeOrder = async (req, res, next) => {
+  try {
+    if (req.session.init) {
+      res.status(412).json({
+        status: 'Pre condition Failed',
+        message: `Please select 99 to checkout your pending order.`,
+      });
+      return;
+    }
+
+    // 1.) count documents and create ID for order placed
+    const count = await Orders.countDocuments();
+    let orderId = count + 1;
+
+    req.session.init = true;
+    req.session.orderId = orderId;
+    console.log(req.session);
+
+    await Orders.create({
+      id: orderId,
+      merchant: req.session.username,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Started an Order',
+      data: {
+        Items,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
