bba33f34c72b3f004b6330a5ff232d8a32fc3807
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-03-07 14:52:41 +0100

    rename server.js to app.js & install express-sessn

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const session = require('express-session');
 
 const orderRouter = require('./routes/orderRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const store = new session.MemoryStore();
 
 require('dotenv').config();
 
@@ -17,7 +21,19 @@ if ((process.env.NODE_ENV = 'development')) {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1/chatbot', orderRouter);
+//  creating in memory sessions for our clients to stay recognized by the server.
+const sessionOptions = {
+  secret: `${process.env.SESSION_SECRET}`,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60 * 60 * 1000 },
+  store,
+};
+
+app.use(session(sessionOptions));
+
+app.use('/api/v1/chatbot/', orderRouter);
+app.use('/api/v1/chatbot/users', userRouter);
 
 app.use('*', (req, res, next) => {
   res.status(404).json({
