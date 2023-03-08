7245978eeacecf2995a9e203514eaea3fb1dcf5f
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-03-08 10:40:30 +0100

    add routes consuming middleware and func handlers

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,13 +3,18 @@ const orderController = require('../controllers/orderController');
 
 const router = express.Router();
 
+// Reforming the router and the router handlers, where should we put the handlers
+// should we access the client replies via the request params or body?
 router
   .route('/')
-  .get(orderController.getInfoAboutChatbot)
-  .post(orderController.placeOrder, orderController.checkoutOrder);
+  .get(orderController.getUserInfo, orderController.getInfoAboutChatbot);
 
-// router.route('/');
+router.route('/1').post(orderController.placeOrder);
+router.route('/99').post(orderController.checkoutOrder);
+router.route('/98').get(orderController.orderHistory);
+router.route('/97').get(orderController.currentOrder);
+router.route('/0').get(orderController.cancelOrder);
 
-router.route('/:id').post(orderController.selectItem);
+router.route('/1/:id').post(orderController.selectItem);
 
 module.exports = router;
