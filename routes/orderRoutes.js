d75e8c4c5980abc9f56aebc2d99aa96d533e6f6c
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-03-17 20:24:40 +0100

    update: routes handling altered

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,16 +5,14 @@ const router = express.Router();
 
 // Reforming the router and the router handlers, where should we put the handlers
 // should we access the client replies via the request params or body?
-router
-  .route('/')
-  .get(orderController.getUserInfo, orderController.getInfoAboutChatbot);
+router.route('/').post(orderController.getInfoAboutChatbot);
 
-router.route('/1').post(orderController.placeOrder);
-router.route('/99').post(orderController.checkoutOrder);
+router.route('/1').get(orderController.placeOrder);
+router.route('/99').get(orderController.checkoutOrder);
 router.route('/98').get(orderController.orderHistory);
 router.route('/97').get(orderController.currentOrder);
 router.route('/0').get(orderController.cancelOrder);
 
-router.route('/1/:id').post(orderController.selectItem);
+router.route('/1/:id').get(orderController.selectItem);
 
 module.exports = router;
