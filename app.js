7c7bec87d86c8ecfa564c86784bd20a41f820b24
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-03-17 20:23:38 +0100

    session implementation and database integration

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+const http = require('http');
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const session = require('express-session');
+const bodyparser = require('body-parser');
+const passport = require('passport');
+const cors = require('cors');
 
 const orderRouter = require('./routes/orderRoutes');
 const userRouter = require('./routes/userRoutes');
 
-const store = new session.MemoryStore();
-
-require('dotenv').config();
+const MongoStore = require('connect-mongo');
 
 require('./database')();
+
 const app = express();
+
+//setting the public folder as the static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // using morgan to log incoming requests' type, in the 'dev' environment
 if ((process.env.NODE_ENV = 'development')) {
@@ -18,21 +25,52 @@ if ((process.env.NODE_ENV = 'development')) {
 }
 
 // using express library to gain access to body of request sent by clients
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+const whitelist = [
+  'https://zeuhz-droid.github.io',
+  'http://localhost:5501',
+  'http://localhost:5500',
+];
+
+//  Implementing CORS
+app.use(
+  cors({
+    origin: whitelist,
+    credentials: true,
+    methods: 'GET, POST',
+  })
+);
 
 //  creating in memory sessions for our clients to stay recognized by the server.
+
+app.set('trust proxy', 1);
+
 const sessionOptions = {
-  secret: `${process.env.SESSION_SECRET}`,
+  name: 'orderBot',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  cookie: { maxAge: 60 * 60 * 1000 },
-  store,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URL,
+    touchAfter: 24 * 3600,
+  }),
+  cookie: {
+    name: 'orderBot',
+    domain: '.zeuhz-droid.github',
+    secure: true,
+    httpOnly: true,
+    sameSite: 'none',
+    maxAge: 24 * 60 * 60 * 1000,
+  },
 };
-
 app.use(session(sessionOptions));
 
-app.use('/api/v1/chatbot/', orderRouter);
+app.use(passport.initialize()); //initialize passport
+app.use(passport.session()); //initialize session with passport
+
+app.use('/api/v1/chatbot', orderRouter);
 app.use('/api/v1/chatbot/users', userRouter);
 
 app.use('*', (req, res, next) => {
