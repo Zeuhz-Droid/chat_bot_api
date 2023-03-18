541ea5487aace8760fa0d066631edb6ec92de2e4
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-03-18 02:34:09 +0100

    Possible Fix: removed domain field

--- app.js
+++ app.js
@@ -59,7 +59,6 @@ const sessionOptions = {
   }),
   cookie: {
     name: 'orderBot',
-    domain: 'zeuhz-orderbotconsumer-droid.netlify.app',
     secure: true,
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000,
