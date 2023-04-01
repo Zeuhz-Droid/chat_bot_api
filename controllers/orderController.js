dbe94a35f1c07dd43a8433fd5a8c3411bf27539f
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-04-01 19:10:32 +0100

    Bug Fix: revamping the way the app handles the "last updated" date. This should fix the issue where the app would show the wrong date for the last update.

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -61,7 +61,7 @@ exports.placeOrder = async (req, res, next) => {
     req.session.orderId = orderId;
 
     await Orders.create({
-      id: req.session.id,
+      id: orderId,
       merchant: req.session.username,
     });
 
@@ -139,7 +139,7 @@ exports.orderHistory = async (req, res, next) => {
       return;
     }
 
-    orders = await Orders.find({ id: req.session.id }).where({
+    orders = await Orders.find({ id: req.session.orderId }).where({
       fulfilled: true,
     });
 
